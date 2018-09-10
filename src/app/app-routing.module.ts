6fc9dc93e469333696fbca1a8ff01fac6075418e
nesar11 <nesar.uddin100gmail.com>
2018-09-10 13:30:14 +0800

    First Route Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes} from  '@angular/router';;
+import {RouterModule} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {RegisterComponent} from './register/register.component';
+import {LoginComponent} from './login/login.component';
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+} , {
+  path: 'register',
+  component: RegisterComponent
+}, {
+  path: 'login',
+  component: LoginComponent
+}
+];
+
+@NgModule({
+  imports: [
+   RouterModule.forRoot(routes)
+  ],
+    exports: [
+      RouterModule
+    ]
+}
+  )
+
+export class AppRoutingModule { }
